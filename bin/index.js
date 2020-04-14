f9ca4c1c10c382ecf42a8765f21bc048e3fe48b0
André Sattler <a.sattler@lokalportal.de>
2020-04-14 19:29:19 +0200

    added cli tool

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+const chalk = require('chalk')
+const marked = require('marked')
+const TerminalRenderer = require('marked-terminal')
+ 
+marked.setOptions({
+  renderer: new TerminalRenderer({
+    tab: 2
+  })
+})
+
+console.log(chalk.red(`
+  ░█▀▀▄░▒█▄░▒█░▒█▀▀▄░▒█▀▀▄░▒█▀▀▀░░░▒█▀▀▀█░█▀▀▄░▀▀█▀▀░▀▀█▀▀░▒█░░░░▒█▀▀▀░▒█▀▀▄
+  ▒█▄▄█░▒█▒█▒█░▒█░▒█░▒█▄▄▀░▒█▀▀▀░░░░▀▀▀▄▄▒█▄▄█░░▒█░░░░▒█░░░▒█░░░░▒█▀▀▀░▒█▄▄▀
+  ▒█░▒█░▒█░░▀█░▒█▄▄█░▒█░▒█░▒█▄▄▄░░░▒█▄▄▄█▒█░▒█░░▒█░░░░▒█░░░▒█▄▄█░▒█▄▄▄░▒█░▒█
+`))
+
+const aboutMd = fs.readFileSync('./src/about.md', 'utf8')
+const workHistoryMd = fs.readFileSync('./src/work-history.md', 'utf8')
+const technicalToolSetMd = fs.readFileSync('./src/technical-toolset.md', 'utf8')
+
+const renderSubHeader = (text) => {
+  console.log(chalk.red.underline(text))
+}
+
+const renderMd = (markdown) => {
+  console.log(marked(markdown))
+} 
+
+renderSubHeader('About')
+renderMd(aboutMd)
+renderSubHeader('Work History:')
+renderMd(workHistoryMd)
+renderSubHeader('Technical Toolset')
+renderMd(technicalToolSetMd)
